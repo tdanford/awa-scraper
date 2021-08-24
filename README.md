c30376c4e903d63925693e5c39eece7742bc18c5
armanye <42426731+armanye@users.noreply.github.com>
2021-08-23 14:37:54 -1000

    Add instructions on how to run it.
    $ python setup.py develop
    $ python -m awa
    2021-08-24T00:38:28.130Z INFO:root:Doing something

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # awa-scraper
+
+## Develop
+```console
+$ python setup.py develop
+$ python -m awa
+2021-08-24T00:38:28.130Z INFO:root:Doing something
+```
---
+++ src/awa/__main__.py
@@ -0,0 +1,18 @@
+import argparse
+import logging
+import time
+
+if __name__ == "__main__":
+    # Always log in UTC and output in RFC3339
+    logging.Formatter.converter = time.gmtime
+
+    formatter = logging.basicConfig(
+        format="%(asctime)s.%(msecs)03dZ %(levelname)s:%(name)s:%(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S",
+        level=logging.INFO,
+    )
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    logging.info('Doing something')
